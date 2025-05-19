02381fd9ed43b6a0ab0d7939b0f0fad271bdcca7
shierlu <3404027211@qq.com>
2025-05-19 20:49:47 +0800

    Site updated: 2025-05-19 20:49:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747658760113";const preCache = ["/images/taichi.png","/images/001.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747658981381";const preCache = ["/images/taichi.png","/images/001.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
